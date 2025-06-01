5c4ef14990cd88eb3e3be6dcbc87a7edd0c2af80
mvaden <mvaden08@gmail.com>
2025-06-01 15:49:54 -0400

    front-end:reformat date-time to be ISO 8601 compliant

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
                 return res.json();
 		})
             .then(({ Message }) => {
-                setMessage(convertDates(Message, 'date'));
+                setMessage(formatDates(Message, 'date'));
             })
             .catch(({ Error }) => {
                 console.error(Error);
@@ -24,12 +24,49 @@ function App() {
 		});
   	}, []);
 
-    console.log("Message: ", message)
+    
+	function formatDates(array, dateKey) {
+		return array.map((item) => {
+			const original = item[dateKey];
+			const [datePart, timePart] = original.split(' ');
+			const [day, month, year] = datePart.split('/'); 
+			const isoString = `${year}-${month}-${day}T${timePart}`;
+
+			return {
+				...item,
+				[dateKey]: new Date(isoString),
+			};
+		});
+	};
   	
 	return (
-		<>
-			hello react
-		</>
+		<div>
+            <h1>Steel Industry Energy Usage</h1>
+            <div style={{ marginBottom: '1rem' }}>
+                
+				<label>
+				    Start Date:
+                    <input 
+                        type="datetime-local" 
+                        value={startDate}
+                        max="01-01-2019"
+                        min="01-01-2018"
+                        onChange={e => setStartDate(e.target.value)} 
+                    />
+                </label>
+				<br />
+				<label style={{ marginLeft: '1rem' }}>
+                    End Date:
+                    <input 
+                        type="datetime-local" 
+                        value={endDate} 
+                        max="2019-01-01T00:00"
+                        min="2018-01-01T00:00"
+                        onChange={e => setEndDate(e.target.value)} 
+                    />
+                </label>
+            </div>
+		</div>
   	)
 };
 
